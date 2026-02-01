d739708a793343f5ad67ca73611f93de3c1cc249
shau-git <k.shaujie@gmail.com>
2026-02-01 13:16:27 +0800

    app.js proxy

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://np-sep-career.vercel.app', 'https://np-sep-career.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add all methods you use
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
