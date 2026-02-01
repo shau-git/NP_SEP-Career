7163ca7bad0baa1db2135cc3bd60e39b21b0bd1e
shau-git <k.shaujie@gmail.com>
2026-02-01 13:10:52 +0800

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        // Your backend port   
+        //target: 'http://localhost:3000', 
+        target: 'https://np-sep-career.onrender.com', 
+        changeOrigin: true,
+      },
+    },
+  },
 })
